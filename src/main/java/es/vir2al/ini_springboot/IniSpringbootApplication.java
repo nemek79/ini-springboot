143f73494fc40562f009d5ac0c6738bbc1412a68
David Alonso <davidalonso79@gmail.com>
2024-09-28 17:24:44 +0200

    dev: codigo inicial del proyecto

---
+++ src/main/java/es/vir2al/ini_springboot/IniSpringbootApplication.java
@@ -0,0 +1,13 @@
+package es.vir2al.ini_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IniSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IniSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/vir2al/ini_springboot/IniSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package es.vir2al.ini_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IniSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
